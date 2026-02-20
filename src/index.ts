e41d567eb3287a61b53fd7f07f354bfe9ef6df77
Dev Dixit <devdixitsocial@gmail.com>
2026-02-20 22:50:47 +0530

    fix: update response message in root endpoint to be more concise

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = 8000;
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({ message: "Classroom backend is running." });
+  res.json({ message: "Backend is running." });
 });
 
 app.listen(PORT, () => {
