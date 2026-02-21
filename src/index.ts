05f15b854756a490e74b1cee4e59dd171dc7c427
Dev Dixit <devdixitsocial@gmail.com>
2026-02-21 21:48:16 +0530

    fix: update test script and package manager version in package.json; refine response message in root endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ const PORT = 8000;
 app.use(express.json());
 
 app.get("/", (_req, res) => {
-  res.json({ message: "Backend is running." });
+  res.json({ message: "Classroom Backend is running..." });
 });
 
 app.listen(PORT, () => {
